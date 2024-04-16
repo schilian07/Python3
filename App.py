e40ec01c01cdb6a36e547a5bc11d9d860f429559
schilian07 <161601193+schilian07@users.noreply.github.com>
2024-04-16 09:12:50 -0300

    U]

---
+++ App.py
@@ -0,0 +1,52 @@
+import os
+
+restaurantes = ["*Leit.ao,"Schilian restaurante"]
+
+def finalizar_app():
+os.system("clear")
+print("Finalizando o app\n")
+
+def escolher_opcoes():
+print("Programa Expresso")
+print("*****************")
+print("1 - Cadastrar restaurante")
+print("2 - Listar restaurante")
+print("3 - Ativar restaurante")
+print("4 - Sair\n")
+
+def opcao_invalida():
+print("Opção inválida\n")
+input("Digite uma tecla para voltar ao menu principal: ")
+main()
+
+def chamar_nome_do_app():
+print("Restaurante Expressao\n")
+
+def cadastrar_novo_restaurante():
+nome = input("Digite o nome do novo restaurante: ")
+restaurantes.append(nome)
+print(f"Você cadastrou o restaurante: {nome}")
+
+def main():
+escolher_opcoes()
+chamar_nome_do_app()
+try:
+opcaodigitada = int(input("Digite a opção desejada: "))
+if opcaodigitada == 1:
+print("Você escolheu cadastrar restaurante\n")
+cadastrar_novo_restaurante()
+elif opcaodigitada == 2:
+print("Você escolheu listar restaurante\n")
+elif opcaodigitada == 3:
+print("Você escolheu ativar restaurante\n")
+elif opcaodigitada == 4:
+print("Você escolheu sair do aplicativo\n")
+finalizar_app()
+else:
+opcao_invalida()
+except ValueError:
+opcao_invalida()
+
+if __name__ == "__main__":
+finalizar_app()
+main()
